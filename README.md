0b66a0665c261db7e7d2eed2aef63dae3347432b
Luke Hansell <luke.hansell@holidayextras.com>
2016-07-12 11:40:09 +0100

    adding instructions

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Example App
+# HX JS Training
+
+A simple app designed to test the basics of javascript in a TDD environment.
+It uses a web worker to run the code that users input against tests and outputs the result to the screen.
 
 ## install
 ```
--- app/components/App.js
+++ app/components/App.js
@@ -24,8 +24,23 @@ export default ({ children }) =>  (
 		<Navbar>
 			<NavbarBrand>Will You Teach Me To JS?</NavbarBrand>
 		</Navbar>
+		<div>
+			<p>
+				Welcome to the HX JS IP test. If you don't know what any of those acronyms mean then you're probably in the wrong place.
+			</p>
+			<p>
+				Below are a set of exercises for you to complete. Each example provides instructions and a space for you to write your code. The tests on the right
+				hand side will run automatically when you change the code in the TDD style. Any output to <code>console.log</code> will be output in the "Console" space.
+			</p>
+			<p>
+				Your job is to make all the test pass. A test is passing when it turns green. Good luck!
+			</p>
+		</div>
 		{lessons.map( (lesson, i ) => (
 			<Lesson key={i} id={i} tests={lesson.tests} defaultCode={lesson.task}/>
 		))}
+		<p>
+			The code for these tests (including the solutions) is available on <a href="https://github.com/lukehansell/JS-training" target="_blank">GitHub</a>
+		</p>
 	</Grid>
 )
